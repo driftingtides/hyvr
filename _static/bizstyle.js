28bd833551b30d739bae55723ddef39c49d69377
driftingtides <jeremypaulbennett@gmail.com>
2018-04-19 13:42:22 +0200

    Added modules to documentation

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("HYVR 0.1 documentation");
+        $("li.nav-item-0 a").text("HYVR 0.2 documentation");
     }
 });
